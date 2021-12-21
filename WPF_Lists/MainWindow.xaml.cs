fb78e0cdd6fbdc5d4518e03914e06cd9b4d3ae58
Starinin Andrey <starininandrey@gmail.com>
2021-12-21 19:17:51 +0300

    Init

---
+++ WPF_Lists/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace WPF_Lists
+{
+    public partial class MainWindow : Window
+    {
+        private List<Phone> _phones;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _phones = new List<Phone>
+            {
+                new Phone()
+                {
+                    Name = "iPhone", 
+                    Manufactured = "Apple", 
+                    Price = 200000
+                },
+                new Phone()
+                {
+                    Name = "Galaxy", 
+                    Manufactured = "Samsung", 
+                    Price = 100000
+                },
+                new Phone()
+                {
+                    Name = "Oppo", 
+                    Manufactured = "China", 
+                    Price = 20000
+                }
+            };
+
+            List_Phones.ItemsSource = _phones;
+        }
+    }
+}
---
+++ WPF_Lists/Phone.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_Lists
+{
+    public record Phone
+    {
+        public string Name { get; init; }
+        public string Manufactured { get; init; }
+        public int Price { get; init; }
+    }
+}
